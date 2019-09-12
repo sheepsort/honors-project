d0034b0d7e035125545fa39f6fcdfe614608b767
Jim <jlgriffith180@gmail.com>
2019-09-12 10:30:14 -0500

    Playing around with tags

--- src/app/models/project.model.ts
+++ src/app/models/project.model.ts
@@ -1,14 +1,18 @@
 export class Project {
     projectName: string;
-    githubUrl?: string;
+    githubUrl: string;
     description: string;
-    id?: number;
+    id: number;
+    tags: string[];
+    tagline: string;
 
     // All Projects should have each of these props when created with the new Project command
-    constructor(name: string, github: string, desc: string, id: number) {
+    constructor(name: string, github: string, desc: string, id: number, tags: string[], tagline: string) {
         this.projectName = name;
         this.githubUrl = github;
         this.description = desc;
         this.id = id;
+        this.tags = tags;
+        this.tagline = tagline;
     }
 }
